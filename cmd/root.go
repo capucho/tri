6b92052d13012fa175ff017a76dfa604ab40e7bd
João Henrique Laranja Capucho <jhcapucho@gmail.com>
2022-01-21 14:17:21 +0100

    253 - start using homedir

--- cmd/root.go
+++ cmd/root.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/mitchellh/go-homedir"
 )
 
 
@@ -46,6 +47,11 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+  home, err := homedir.Dir()
+  if err != nil {
+
+  }
 }
 
 
